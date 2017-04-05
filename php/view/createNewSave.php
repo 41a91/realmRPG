94877db65f8189134dcad6531fe376da9ee8b398
Andrew Phelps <phelpsa@msjmath.loc>
2017-04-05 15:44:03 -0400

    worked on some php

--- php/view/createNewSave.php
+++ php/view/createNewSave.php
@@ -5,7 +5,7 @@
     <title>Title</title>
 </head>
 <body>
-<form id="newSave" action="/realmRPG/php/control/newSaveControl.php" method="post">
+<form id="newSave" action="../realmRPG/php/control/newSaveControl.php" method="post">
     <label>Username: <input type="text" id="username" name="username" required="required"/></label><br/>
     <label>Password: <input type="password" id="password" name="password" required="required"/></label>
     <input type="submit" id="submit" name="submit" value="Submit"/>
